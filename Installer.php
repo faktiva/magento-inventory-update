45f0923c9423d3c865468b0a139243268e59da7e
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2016-09-30 14:26:57 +0200

    Added installer

---
+++ Installer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace J4T\MagentoInventoryUpdate;
+
+use Composer\Script\Event;
+
+class Installer
+{
+    public static function install(Event $event)
+    {
+        $extras = $event->getComposer()->getPackage()->getExtra();
+        $install_dir = $extras['magento-scripts-dir'];
+
+        copy(__DIR__.'/shell/inventory_update.php', "./$install_dir/inventory_update.php");
+    }
+}
