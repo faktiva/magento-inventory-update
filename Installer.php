884532facab215d8f0ef3b14b70ec734bafcdde8
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2016-10-03 10:19:55 +0200

    Throw an Exception missing 'magento-scripts-dir'

--- Installer.php
+++ Installer.php
@@ -9,6 +9,9 @@ class Installer
     public static function install(Event $event)
     {
         $extras = $event->getComposer()->getPackage()->getExtra();
+        if ( empty($extras['magento-scripts-dir']) ) {
+            throw new \Exception('Missing "magento-scripts-dir" in composer.json "extra"');
+        }
         $install_dir = $extras['magento-scripts-dir'];
 
         copy(__DIR__.'/shell/inventory_update.php', "./$install_dir/inventory_update.php");
